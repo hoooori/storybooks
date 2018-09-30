97dc231da2ed54459a712c495c69fdfe3c1d48bf
hoooori <hideyasu.horiuchi@gmail.com>
2018-09-30 14:48:17 +0900

    feat:installed session and cookie-parser and implemented verify/logout

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
-const express  = require('express');
-const mongoose = require('mongoose');
-const passport = require('passport');
-const auth     = require('./routes/auth');
-const keys     = require('./config/keys');
-const app      = express();
-const port     = process.env.PORT || 5000;
+const express      = require('express');
+const mongoose     = require('mongoose');
+const passport     = require('passport');
+const auth         = require('./routes/auth');
+const keys         = require('./config/keys');
+const cookieParser = require('cookie-parser');
+const session      = require('express-session');
+const app          = express();
+const port         = process.env.PORT || 5000;
 
 require('./models/User');
 require('./config/passport')(passport);
 
+app.use(cookieParser());
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(passport.initialize());
+app.use(passport.session());
+// Set global vars
+app.use((req, res, next) => {
+  res.locals.user = req.user || null;
+  next();
+})
 app.use('/auth', auth);
 
 // ******** MongoDB Connection ******** //
@@ -17,6 +28,8 @@ mongoose.connect(keys.mongoURI, { useNewUrlParser: true }) // Connect
 .then(() => console.log('MongoDB Connected...')) // when success
 .catch(err => console.log(err)); // when fail
 // ******** MongoDB Connection ******** //
+
+app.get('/', (req, res) => { res.send('It Works!'); })
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`)
