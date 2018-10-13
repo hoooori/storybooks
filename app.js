54e58d209c65915b0479f2a2eae383f6d4e99ed4
hoooori <hideyasu.horiuchi@gmail.com>
2018-10-13 20:45:24 +0900

    feat:created index routes and header&navbar

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 const express      = require('express');
+const exphbs       = require('express-handlebars');
 const mongoose     = require('mongoose');
 const passport     = require('passport');
-const auth         = require('./routes/auth');
 const keys         = require('./config/keys');
 const cookieParser = require('cookie-parser');
 const session      = require('express-session');
 const app          = express();
 const port         = process.env.PORT || 5000;
 
+// Load Routes
+const index = require('./routes/index');
+const auth  = require('./routes/auth');
+
 require('./models/User');
 require('./config/passport')(passport);
 
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 app.use(cookieParser());
 app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
 app.use(passport.initialize());
@@ -20,6 +27,9 @@ app.use((req, res, next) => {
   res.locals.user = req.user || null;
   next();
 })
+
+// User Routes
+app.use('/', index);
 app.use('/auth', auth);
 
 // ******** MongoDB Connection ******** //
@@ -28,8 +38,6 @@ mongoose.connect(keys.mongoURI, { useNewUrlParser: true }) // Connect
 .then(() => console.log('MongoDB Connected...')) // when success
 .catch(err => console.log(err)); // when fail
 // ******** MongoDB Connection ******** //
-
-app.get('/', (req, res) => { res.send('It Works!'); })
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`)
